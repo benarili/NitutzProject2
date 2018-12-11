5f078c669a7bd163ca72e34f3f0c305ee8e5cb0d
itzikvais <yizhakva@post.bgu.ac.il>
2018-12-11 11:50:55 +0200

    user

--- src/User/User.java
+++ src/User/User.java
@@ -1,7 +1,30 @@
 package User;
+import DataBase.*;
+
+import java.util.ArrayList;
 
 public class User {
-    public void createNewUser(String username,String pass ,String email,String name, String lastname, String birthDath){
+    private String username;
+    private String pass;
+    private String email;
+    private String name;
+    private String lastname;
+    private String birthDath;
+    private dbTableUsers db=new dbTableUsers();
 
+    public User(String username, String pass, String email, String name, String lastname, String birthDath) {
+        this.username = username;
+        this.pass = pass;
+        this.email = email;
+        this.name = name;
+        this.lastname = lastname;
+        this.birthDath = birthDath;
+    }
+    public boolean createNewUser(){
+        String[] insert={username,pass,email,name,lastname,birthDath};
+        return db.InsertComand( "INSERT INTO users(username,password,email,name,last_name,birth_date,image) VALUES(?,?,?,?,?,?,?)",insert );
+    }
+    public ArrayList<String> selectUser(String query, String[] fields){
+        return db.selectCommand( query,fields );
     }
 }
